ed50ad6ec02a3b22971f5b152086384912723d7a
David Bors <davidbors@icloud.com>
2021-12-04 19:56:08 +0200

    Added opcodes, push and pop auxiliary functions, and logic flags function

--- src/emulator/8080.c
+++ src/emulator/8080.c
@@ -107,13 +107,49 @@ void write_to_hl (uint8_t val)
 	uint16_t mem_addr = (h << 8) | l;
 	write_mem(mem_addr, val);
 }
-	
+
+/*
+ * set flags after an arithmetic operation
+ */
 void arith_flags (uint16_t val)
 {
 	flags.cy = (val > 0xff);
 	flags.z  = ((val & 0xff) == 0);
 	flags.s  = (0x80 == (val & 0x80));
 	flags.p  = parity(val & 0xff, 8);
+}
+
+/*
+ * set flags after a logic operation
+ */
+void logic_flags ()
+{
+	flags.cy = flags.ac = 0;
+	flags.z  = (a == 0);
+	flags.s  = (0x80 == (a & 0x80));
+	flags.p  = parity(a, 8);
+}
+
+/*
+ * pop pair of registers from the stack
+ * (basically from the memory address pointed
+ * by the stack pointer)
+ */
+void pop (uint8_t *high, uint8_t *low)
+{
+	*low  = memory[sp];
+	*high = memory[sp+1];
+	sp += 2;
+}
+
+/*
+ * push pair of registers in the stack
+ */
+void push (uint8_t high, uint8_t low)
+{
+	write_mem(sp-1, high);
+	write_mem(sp-2, low);
+	sp -= 2;
 }
 
 void emulate8080 ()
@@ -451,5 +487,695 @@ void emulate8080 ()
 	case 0x38:
 		unknown_instruction();
 		break;
+		/* DAD SP */
+	case 0x39:
+	{
+		uint32_t hl = (h << 8) | l;
+		uint32_t result = hl + sp;
+		h = (result & 0xff00) >> 8;
+		l = result & 0xff00;
+		flags.cy = ((result & 0xffff0000) > 0);
 	}
+	case 0x3a:
+	{
+		uint16_t mem_addr = (opcode[2] << 8) | opcode[1];
+		a = memory[mem_addr];
+		pc += 2;
+	}
+	/* DCX SP */
+	case 0x3b:
+		sp--;
+		break;
+		/* INR A */
+	case 0x3c:
+		a++;
+		flags_zsp(a);
+		break;
+		/* DCR A */
+	case 0x3d:
+		a--;
+		flags_zsp(a);
+		break;
+		/* MVI A, byte */
+	case 0x3e:
+		a = opcode[1];
+		pc++;
+		break;
+		/* MOV B, ? */
+	case 0x40: b = b; break;
+	case 0x41: b = c; break;
+	case 0x42: b = d; break;
+	case 0x43: b = e; break;
+	case 0x44: b = h; break;
+	case 0x45: b = l; break;
+	case 0x46: b = read_from_hl(); break;
+	case 0x47: b = a; break;
+		/* MOV C, ? */
+	case 0x48: c = b; break;
+	case 0x49: c = c; break;
+	case 0x4a: c = d; break;
+	case 0x4b: c = e; break;
+	case 0x4c: c = h; break;
+	case 0x4d: c = l; break;
+	case 0x4e: c = read_from_hl(); break;
+	case 0x4f: c = a; break;
+		/* MOV D, ? */
+	case 0x50: d = b; break;
+	case 0x51: d = c; break;
+	case 0x52: d = d; break;
+	case 0x53: d = e; break;
+	case 0x54: d = h; break;
+	case 0x55: d = l; break;
+	case 0x56: d = read_from_hl(); break;
+	case 0x57: d = a; break;
+		/* MOV E, ? */
+	case 0x58: e = b; break;
+	case 0x59: e = c; break;
+	case 0x5a: e = d; break;
+	case 0x5b: e = e; break;
+	case 0x5c: e = h; break;
+	case 0x5d: e = l; break;
+	case 0x5e: e = read_from_hl(); break;
+	case 0x5f: e = a; break;
+		/* MOV H, ? */
+	case 0x60: h = b; break;
+	case 0x61: h = c; break;
+	case 0x62: h = d; break;
+	case 0x63: h = e; break;
+	case 0x64: h = h; break;
+	case 0x65: h = l; break;
+	case 0x66: h = read_from_hl(); break;
+	case 0x67: h = a; break;
+		/* MOV L, ? */
+	case 0x68: l = b; break;
+	case 0x69: l = c; break;
+	case 0x6a: l = d; break;
+	case 0x6b: l = e; break;
+	case 0x6c: l = h; break;
+	case 0x6d: l = l; break;
+	case 0x6e: l = read_from_hl(); break;
+	case 0x6f: l = a; break;
+		/* MOV M, ? */
+	case 0x70: write_to_hl(b); break;
+	case 0x71: write_to_hl(c); break;
+	case 0x72: write_to_hl(d); break;
+	case 0x73: write_to_hl(e); break;
+	case 0x74: write_to_hl(h); break;
+	case 0x75: write_to_hl(l); break;
+	case 0x76: break;
+	case 0x77: write_to_hl(a); break;
+		/* MOV A, ? */
+	case 0x78: a = b; break;
+	case 0x79: a = c; break;
+	case 0x7a: a = d; break;
+	case 0x7b: a = e; break;
+	case 0x7c: a = h; break;
+	case 0x7d: a = l; break;
+	case 0x7e: a = read_from_hl(); break;
+	case 0x7f: a = a; break;
+		/* ADD ? */
+	case 0x80: { uint16_t result = (uint16_t) a + (uint16_t) b;
+			arith_flags(result); a = (result & 0xff); break; }
+	case 0x81: { uint16_t result = (uint16_t) a + (uint16_t) c;
+			arith_flags(result); a = (result & 0xff); break; }
+	case 0x82: { uint16_t result = (uint16_t) a + (uint16_t) d;
+			arith_flags(result); a = (result & 0xff); break; }
+	case 0x83: { uint16_t result = (uint16_t) a + (uint16_t) e;
+			arith_flags(result); a = (result & 0xff); break; }
+	case 0x84: { uint16_t result = (uint16_t) a + (uint16_t) h;
+			arith_flags(result); a = (result & 0xff); break; }
+	case 0x85: { uint16_t result = (uint16_t) a + (uint16_t) l;
+			arith_flags(result); a = (result & 0xff); break; }
+	case 0x86: { uint16_t result = (uint16_t) a + (uint16_t) read_from_hl();
+			arith_flags(result); a = (result & 0xff); break; }
+	case 0x87: { uint16_t result = (uint16_t) a + (uint16_t) a;
+			arith_flags(result); a = (result & 0xff); break; }
+
+		/* ADC ? */
+	case 0x88:
+	{
+		uint16_t result = (uint16_t) a + (uint16_t) b +
+			flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x89:
+	{
+		uint16_t result = (uint16_t) a + (uint16_t) c +
+			flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x8a:
+	{
+		uint16_t result = (uint16_t) a + (uint16_t) d +
+			flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x8b:
+	{
+		uint16_t result = (uint16_t) a + (uint16_t) e +
+			flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x8c:
+	{
+		uint16_t result = (uint16_t) a + (uint16_t) h +
+			flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x8d:
+	{
+		uint16_t result = (uint16_t) a + (uint16_t) l +
+			flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x8e:
+	{
+		uint16_t result = (uint16_t) a + (uint16_t) read_from_hl() +
+			flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x8f:
+	{
+		uint16_t result = (uint16_t) a + (uint16_t) a +
+			flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	/* SUB ? */
+	case 0x90:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) b;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x91:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) c;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x92:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) d;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x93:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) e;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x94:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) h;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x95:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) l;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x96:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) read_from_hl();
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x97:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) a;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	/* SBB ? */
+	case 0x98:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) b - flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x99:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) c - flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x9a:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) d - flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x9b:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) e - flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x9c:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) h - flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x9d:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) l - flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x9e:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) read_from_hl() - flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	case 0x9f:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) a - flags.cy;
+		arith_flags(result);
+		a = (result & 0xff);
+		break;
+	}
+	/* ANA ? */
+	case 0xa0:
+		a = a & b;
+		logic_flags();
+		break;
+	case 0xa1:
+		a = a & c;
+		logic_flags();
+		break;
+	case 0xa2:
+		a = a & d;
+		logic_flags();
+		break;
+	case 0xa3:
+		a = a & e;
+		logic_flags();
+		break;
+	case 0xa4:
+		a = a & h;
+		logic_flags();
+		break;
+	case 0xa5:
+		a = a & l;
+		logic_flags();
+		break;
+	case 0xa6:
+		a = a & read_from_hl();
+		logic_flags();
+		break;
+	case 0xa7:
+		a = a & a;
+		logic_flags();
+		break;
+		/* XRA ? */
+	case 0xa8:
+		a ^= b;
+		logic_flags();
+		break;
+	case 0xa9:
+		a ^= c;
+		logic_flags();
+		break;
+	case 0xaa:
+		a ^= d;
+		logic_flags();
+		break;
+	case 0xab:
+		a ^= e;
+		logic_flags();
+		break;
+	case 0xac:
+		a ^= h;
+		logic_flags();
+		break;
+	case 0xad:
+		a ^= l;
+		logic_flags();
+		break;
+	case 0xae:
+		a ^= read_from_hl();
+		logic_flags();
+		break;
+	case 0xaf:
+		a ^= a;
+		logic_flags();
+		break;
+		/* ORA ? */
+		/* ^ not a Jojo reference */
+	case 0xb0:
+		a |= b;
+		logic_flags();
+		break;
+	case 0xb1:
+		a |= c;
+		logic_flags();
+		break;
+	case 0xb2:
+		a |= d;
+		logic_flags();
+		break;
+	case 0xb3:
+		a |= e;
+		logic_flags();
+		break;
+	case 0xb4:
+		a |= h;
+		logic_flags();
+		break;
+	case 0xb5:
+		a |= l;
+		logic_flags();
+		break;
+	case 0xb6:
+		a |= read_from_hl();
+		logic_flags();
+		break;
+	case 0xb7:
+		a |= a;
+		logic_flags();
+		break;
+		/* CMP ? */
+	case 0xb8:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) b;
+		arith_flags(result);
+		break;
+	}
+	case 0xb9:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) c;
+		arith_flags(result);
+		break;
+	}
+	case 0xba:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) d;
+		arith_flags(result);
+		break;
+	}
+	case 0xbb:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) e;
+		arith_flags(result);
+		break;
+	}
+	case 0xbc:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) h;
+		arith_flags(result);
+		break;
+	}
+	case 0xbd:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) l;
+		arith_flags(result);
+		break;
+	}
+	case 0xbe:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) read_from_hl();
+		arith_flags(result);
+		break;
+	}
+	case 0xbf:
+	{
+		uint16_t result = (uint16_t) a - (uint16_t) a;
+		arith_flags(result);
+		break;
+	}
+	/* RNZ */
+	case 0xc0:
+		if (!flags.z) {
+			pc = memory[sp] | (memory[sp+1] << 8);
+			sp += 2;
+		}
+		break;
+		/* POP B */
+	case 0xc1:
+		pop(&b, &c);
+		break;
+		/* JNZ addr */
+	case 0xc2:
+		if (!flags.z)
+			pc = (opcode[2] << 8) | opcode[1];
+		else
+			pc += 2;
+		break;
+		/* JMP addr */
+	case 0xc3:
+		pc = (opcode[2] << 8) | opcode[1];
+		break;
+		/* CNZ addr */
+	case 0xc4:
+		if (!flags.z) {
+			/* will return to the next instruction */
+			uint16_t return_addr = pc + 2;
+			write_mem(sp-1, (return_addr >> 8) & 0xff);
+			write_mem(sp-2, (return_addr & 0xff));
+			sp = sp - 2;
+			pc = (opcode[2] << 8) | opcode[1];
+		} else
+			pc += 2;
+		break;
+		/* PUSH B */
+	case 0xc5:
+		push(b, c);
+		break;
+		/* ADI byte */
+	case 0xc6:
+	{
+		uint16_t result = (uint16_t) a + (uint16_t) opcode[1];
+		flags_zsp(result & 0xff);
+		flags.cy = (result > 0xff);
+		a = result & 0xff;
+		pc++;
+		break;
+	}
+	/* RST 0 */
+	case 0xc7:
+	{
+		uint16_t return_addr = pc + 2;
+		write_mem(sp-1, (return_addr >> 8) & 0xff);
+		write_mem(sp-2, (return_addr & 0xff));
+		sp -= 2;
+		pc = 0x0000;
+		break;
+	}
+	/* RZ */
+	case 0xc8:
+		if (flags.z) {
+			pc = memory[sp] | (memory[sp+1] << 8);
+			sp += 2;
+		}
+		break;
+		/* RET */
+	case 0xc9:
+		pc = memory[sp] | (memory[sp + 1] << 8);
+		sp += 2;
+		break;
+		/* JZ addr */
+	case 0xca:
+		if (flags.z) {
+			pc = (opcode[2] << 8) | opcode[1];
+		} else
+			pc += 2;
+		break;
+	case 0xcb:
+		unknown_instruction();
+		break;
+		/* CZ addr */
+	case 0xcc:
+		if (flags.z == 1) {
+			uint16_t return_addr = pc + 2;
+			write_mem(sp-1, (return_addr >> 8) & 0xff);
+			write_mem(sp-2, (return_addr & 0xff));
+			sp -= 2;
+			pc = (opcode[2] << 8) | opcode[1];
+		} else
+			pc += 2;
+		break;
+		/* CALL addr */
+	case 0xcd:
+	{
+		uint16_t return_addr = pc + 2;
+		write_mem(sp-1, (return_addr >> 8) & 0xff);
+		write_mem(sp-2, (return_addr & 0xff));
+		sp -= 2;
+		pc = (opcode[2] << 8) | opcode[1];
+		break;
+	}
+	/* ACI byte */
+	case 0xce:
+	{
+		uint16_t result = a + opcode[1] + flags.cy;
+		flags_zsp(result & 0xff);
+		flags.cy = (result > 0xff);
+		a = result & 0xff;
+		pc++;
+	}
+	/* RST 1 */
+	case 0xcf:
+	{
+		uint16_t return_addr = pc + 2;
+		write_mem(sp-1, (return_addr >> 8) & 0xff);
+		write_mem(sp-2, (return_addr & 0xff));
+		sp -= 2;
+		pc = 0x0008;
+		break;
+	}
+	/* RNC */
+	case 0xd0:
+		if (!flags.cy) {
+			pc = memory[sp] | (memory[sp+1] << 8);
+			sp += 2;
+		}
+		break;
+		/* POP D */
+	case 0xd1:
+		pop(&d, &e);
+		break;
+		/* JNC */
+	case 0xd2:
+		if (!flags.cy)
+			pc = (opcode[2] << 8) | opcode[1];
+		else
+			pc += 2;
+		break;
+		/* OUT d8 */
+	case 0xd3:
+		pc++;
+		break;
+		/* CNC addr */
+	case 0xd4:
+		if (!flags.cy) {
+			uint16_t return_addr = pc + 2;
+			write_mem(sp-1, (return_addr >> 8) & 0xff);
+			write_mem(sp-2, (return_addr & 0xff));
+			sp -= 2;
+			pc = (opcode[2] << 8) | opcode[1];
+		} else
+			pc += 2;
+		break;
+		/* PUSH D */
+	case 0xd5:
+		push(d, e);
+		break;
+		/* SUI byte */
+	case 0xd6:
+	{
+		uint8_t result = a - opcode[1];
+		flags_zsp(result & 0xff);
+		flags.cy = (a < opcode[1]);
+		a = result;
+		pc++;
+		break;
+	}
+	/* RST 2 */
+	case 0xd7:
+	{
+		uint16_t return_addr = pc + 2;
+		write_mem(sp-1, (return_addr >> 8) & 0xff);
+		write_mem(sp-2, (return_addr & 0xff));
+		sp -= 2;
+		pc = 0x10;
+		break;
+	}
+	/* RN */
+	case 0xd8:
+		if (flags.cy) {
+			pc = memory[sp] | (memory[sp+1] << 8);
+			sp += 2;
+		}
+		break;
+	case 0xd9:
+		unknown_instruction();
+		/* JC */
+	case 0xda:
+		if (flags.cy)
+			pc = (opcode[2] << 8) | opcode[1];
+		else
+			pc += 2;
+		break;
+		/* IN d8 */
+	case 0xdb:
+		pc++;
+		break;
+		/* CC addr */
+	case 0xdc:
+		if (flags.cy) {
+			uint16_t return_addr = pc + 2;
+			write_mem(sp-1, (return_addr >> 8) & 0xff);
+			write_mem(sp-2, (return_addr & 0xff));
+			sp -= 2;
+			pc = (opcode[2] << 8) | opcode[1];
+		} else
+			pc += 2;
+		break;
+	case 0xdd:
+		unknown_instruction();
+		break;
+		/* SBI byte */
+	case 0xde:
+	{
+		uint16_t result = a - opcode[1] - flags.cy;
+		flags_zsp(result & 0xff);
+		flags.cy = (result > 0xff);
+		a = result & 0xff;
+		pc++;
+		break;
+	}
+		/* RST 3 */
+	case 0xdf:
+	{
+		uint16_t return_addr = pc + 2;
+		write_mem(sp-1, (return_addr >> 8) & 0xff);
+		write_mem(sp-2, (return_addr & 0xff));
+		sp -= 2;
+		pc = 0x18;
+		break;
+	}
+	}
+}
+
+
+int main (void)
+{
+	printf("MMN 8080 Emulator\n");
+	return 0;
 }
